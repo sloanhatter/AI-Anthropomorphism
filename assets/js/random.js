ac91f0f487e253ae2952731697de4a4d4b4b6867
sloanhatter <150696213+sloanhatter@users.noreply.github.com>
2025-10-29 15:53:00 -0400

    Update random.js

--- assets/js/random.js
+++ assets/js/random.js
@@ -1,15 +1,14 @@
 window.randomPage = function () {
   const pages = [
-    'Test%20Pages/page1.html',
-    'Test%20Pages/page2.html',
-    'Test%20Pages/page3.html',
-    'Test%20Pages/page4.html',
-    'Test%20Pages/page5.html',
-    'Test%20Pages/page6.html',
-    'Test%20Pages/page7.html',
-    'Test%20Pages/page8.html'
+    'Test Pages/page1.html',
+    'Test Pages/page2.html',
+    'Test Pages/page3.html',
+    'Test Pages/page4.html',
+    'Test Pages/page5.html',
+    'Test Pages/page6.html',
+    'Test Pages/page7.html',
+    'Test Pages/page8.html'
   ];
   const i = Math.floor(Math.random() * pages.length);
-  location.href = pages[i];
+  location.href = encodeURI(pages[i]); // encodes the space to %20
 };
-
